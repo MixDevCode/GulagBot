c4aeed65299afc5e485824a19e0bdd894abc9c6d
Lucas <66272629+MixDevCode@users.noreply.github.com>
2022-03-07 19:26:16 -0300

    Add password to mysql

--- Root/Constants/mysql.js
+++ Root/Constants/mysql.js
@@ -1,10 +1,21 @@
 const mysql = require('mysql');
 const botConfig = require('../../Config.js')
 module.exports = () => {
-	const con = mysql.createConnection({
-		host: botConfig.dbhost,
-		user: botConfig.dbuser,
-		database: botConfig.dbname
-	});
-    return con;
+	if(botConfig.dbpass.length < 1) {
+		const con = mysql.createConnection({
+			host: botConfig.dbhost,
+			user: botConfig.dbuser,
+			database: botConfig.dbname
+		});
+		return con;
+	} else {
+		cosnt con = mysql.createConnection({
+			host: botConfig.dbhost,
+			user: botConfig.dbuser,
+			password: botConfig.dbpass,
+			database: botConfig.dbname
+		});
+		return con;
+	}
+    
 }
